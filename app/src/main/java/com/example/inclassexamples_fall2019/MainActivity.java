52dac9168b6f4139919865fa686f82aa959bfe2c
Eric Torunski <etorunski@gmail.com>
2019-09-09 14:15:32 -0400

    Changes for week 2

--- app/src/main/java/com/example/inclassexamples_fall2019/MainActivity.java
+++ app/src/main/java/com/example/inclassexamples_fall2019/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.inclassexamples_fall2019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //find button 1 from what was loaded, add a click listener
+        Button b1 = findViewById(R.id.button1);
+        if(b1 != null)
+            b1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.i("Message", "You clicked button 1");
+                }
+            });
+
+        //use a Lambda function to set a click listener
+        Button b2 = (Button)findViewById(R.id.button2);
+        b2.setOnClickListener( clk -> Log.i("Message", "You clicked button 1"));
     }
 }
