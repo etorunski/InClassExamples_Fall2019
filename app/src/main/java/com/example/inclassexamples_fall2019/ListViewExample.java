a02d02aaa6097d891a0c02b7ce47799c3d36a4c5
Eric Torunski <etorunski@gmail.com>
2019-09-25 14:40:02 -0400

    Week 4 - talking about ListAdapters

---
+++ app/src/main/java/com/example/inclassexamples_fall2019/ListViewExample.java
@@ -0,0 +1,47 @@
+package com.example.inclassexamples_fall2019;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class ListViewExample extends AppCompatActivity {
+    //items to display
+    ArrayList<String> objects = new ArrayList<>(Arrays.asList("Item 1", "Item 2", "Item 3") );
+
+    BaseAdapter myAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+
+        //You only need 2 lines in onCreate to actually display data:
+        ListView theList = findViewById(R.id.theList);
+        theList.setAdapter( new MyListAdapter() );
+    }
+
+
+
+    //Need to add 4 functions here:
+    private class MyListAdapter extends BaseAdapter {
+
+        public int getCount() { return objects.size();} //This function tells how many objects to show
+
+        public Object getItem(int position) { return objects.get(position);}  //This returns the string at position p
+
+        public long getItemId(int p) { return 0;} //This returns the database id of the item at position p
+
+        public View getView(int p, View recycled, ViewGroup parent)
+        {
+            return ;
+        }
+    }
+}
