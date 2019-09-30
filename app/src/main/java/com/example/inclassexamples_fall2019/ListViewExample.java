df2af97839437283d9f8c7b26da796c490140bbc
Eric Torunski <etorunski@gmail.com>
2019-09-30 16:06:04 -0400

    middle of ListViewExamples

--- app/src/main/java/com/example/inclassexamples_fall2019/ListViewExample.java
+++ app/src/main/java/com/example/inclassexamples_fall2019/ListViewExample.java
@@ -4,7 +4,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import java.util.ArrayList;
@@ -13,7 +18,7 @@ import java.util.Arrays;
 
 public class ListViewExample extends AppCompatActivity {
     //items to display
-    ArrayList<String> objects = new ArrayList<>(Arrays.asList("Item 1", "Item 2", "Item 3") );
+    ArrayList<String> objects = new ArrayList<>(Arrays.asList("Item 1", "Item 2", "Item 3" ) );
 
     BaseAdapter myAdapter;
 
@@ -25,7 +30,20 @@ public class ListViewExample extends AppCompatActivity {
         //You only need 2 lines in onCreate to actually display data:
         ListView theList = findViewById(R.id.theList);
         theList.setAdapter( myAdapter = new MyListAdapter() );
+        theList.setOnItemLongClickListener( ( lv, vw, pos, id) ->{
 
+            Toast.makeText( ListViewExample.this,
+                    "You clicked on:" + pos, Toast.LENGTH_SHORT).show();
+
+            return true;
+        } );
+
+        Button addButton = findViewById(R.id.addButton);
+        addButton.setOnClickListener( clik ->
+                {
+                    objects.add("Item " + (1+objects.size()) );
+                    myAdapter.notifyDataSetChanged(); //update yourself
+                });
     }
 
 
@@ -33,16 +51,24 @@ public class ListViewExample extends AppCompatActivity {
     //Need to add 4 functions here:
     private class MyListAdapter extends BaseAdapter {
 
-        public int getCount() {  return objects.size();  } //This function tells how many objects to show
+        public int getCount() {
+            return objects.size();  } //This function tells how many objects to show
 
-        public String getItem(int position) { return objects.get(position);  }  //This returns the string at position p
+        public String getItem(int position) {
+            return objects.get(position);  }  //This returns the string at position p
 
-        public long getItemId(int p) { return p; } //This returns the database id of the item at position p
+        public long getItemId(int p) {
+            return p; } //This returns the database id of the item at position p
 
         public View getView(int p, View recycled, ViewGroup parent)
         {
-            View thisRow = recycled;
+            View thisRow = getLayoutInflater().inflate(R.layout.table_row_layout, null);
 
+            TextView itemText = thisRow.findViewById(R.id.itemField  );
+            itemText.setText( "Array at:" + p + " is " + getItem(p) );
+
+            TextView numberText = thisRow.findViewById(R.id.numberField);
+            numberText.setText("Your number is " + p);
             return thisRow;
         }
     }
